d507524e48971bc8a3356681476d79390b879bd9
Seth <skeror@gmail.com>
2019-11-21 17:02:08 +0800

    original

---
+++ class-p01Tests/class_p01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  class_p01Tests.swift
+//  class-p01Tests
+//
+//  Created by Seth on 2019/11/20.
+//  Copyright © 2019 Seth. All rights reserved.
+//
+
+import XCTest
+@testable import class_p01
+
+class class_p01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
